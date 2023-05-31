5b47b9c1fa974c6306ef29d64c224c0af61361d3
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-05-31 16:47:16 +0300

    HomeworkJavaCore_01_002 added

---
+++ HomeworkJavaCore_01_002/src/Worker.java
@@ -0,0 +1,27 @@
+public class Worker {
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(int task);
+    }
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i )
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
